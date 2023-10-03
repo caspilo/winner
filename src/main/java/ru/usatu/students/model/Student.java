20b86731083a5d1f10c40b928b1b75745156904d
caspilo <usov.vanya@bk.ru>
2023-10-03 09:25:28 +0500

    1

---
+++ src/main/java/ru/usatu/students/model/Student.java
@@ -0,0 +1,19 @@
+package ru.usatu.students.model;
+
+public class Student {
+    private int id;
+    private String name;
+
+    public Student(){
+    }
+
+    public Student(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    public int getId(){return id;}
+    public String getName(){return name;}
+
+    public void setName(String name){this.name = name;}
+    public void setId(int id){this.id = id;}
+}
